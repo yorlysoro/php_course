2108a4dd0fce4d9a7a7db5d31750a7bf24954402
yorlysoro <yorlysoro@gmail.com>
2023-08-31 21:44:42 -0400

    [ADD] new changes

--- php_course/hello.php
+++ php_course/hello.php
@@ -1,2 +1,7 @@
 <?php
-echo "Hello World!";
+$firstname = "Yorlys";
+echo "Hello World! " . $firstname . "<br />" . "Hello 2";
+
+define('STATUS_PAID', 'paid');
+
+echo '<p>'.  STATUS_PAID . '</p>';
