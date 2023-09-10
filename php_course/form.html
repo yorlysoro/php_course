18ae29d128c6902ca122c58c61a5f8ec00952383
yorlysoro <yorlysoro@gmail.com>
2023-09-10 15:53:56 -0400

    [ADD] data types first form

---
+++ php_course/form.html
@@ -0,0 +1,9 @@
+<form action="action.php" method="post">
+    <label for="name">Your name:</label>
+    <input name="name" id="name" type="text">
+
+    <label for="age">Your age:</label>
+    <input name="age" id="age" type="number">
+
+    <button type="submit">Submit</button>
+</form>
--- php_course/hello.php
+++ php_course/hello.php
@@ -5,3 +5,49 @@ echo "Hello World! " . $firstname . "<br />" . "Hello 2";
 define('STATUS_PAID', 'paid');
 
 echo '<p>'.  STATUS_PAID . '</p>';
+
+//phpinfo();
+
+echo $_SERVER['HTTP_USER_AGENT'];
+
+if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+    echo 'You are using Firefox.';
+}
+
+if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Firefox')) {
+?>
+<h3>str_contains() returned true</h3>
+<p>You are using Firefox</p>
+<?php
+} else {
+?>
+<h3>str_contains() returned false</h3>
+<p>You are not using Firefox</p>
+<?php
+}
+?>
+
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE ||
+    strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) {
+    echo 'You are using Internet Explorer.<br />';
+}
+?>
+
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'Edg') !== FALSE) {
+     echo 'You are using Edge.<br />';
+ }
+?>
+
+<?php echo "Some text <br />"; ?>
+No newline
+<?= "But newline now <br />" ?>
+
+<?php
+    echo 'This is a test <br />'; // This is a one-line c++ style comment
+    /* This is a multi line comment
+       yet another line of comment */
+    echo 'This is yet another test < br/>';
+    echo 'One Final Test < br/>'; # This is a one-line shell-style comment
+?>
